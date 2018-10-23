27d9fef5072b075ceefa7bccfd2f24949ad435be
Kalvin Manson <kalvinmanson@gmail.com>
2018-10-23 16:16:44 -0500

    fix on edit behaviour

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -50,33 +50,32 @@ class ClientController extends Controller
 
   public function update(Request $request, $id)
   {
-    $seller = Seller::findOrFail($id);
+    $client = Client::findOrFail($id);
     $validatedData = $request->validate([
+      'seller_id' => 'required|integer',
       'first_name' => 'required|string|max:50',
       'last_name' => 'required|string|max:50',
-      'document_type' => 'required|max:4',
-      'document' => 'required|integer',
-      'email' => 'required|unique:sellers,email,'.$seller->id.'|max:60',
-      'admission' => 'required|date'
+      'phone' => 'required|max:14',
+      'address' => 'required|max:100',
+      'email' => 'required|unique:clients,email,'.$client->id.'|max:60'
     ]);
-    $seller->user_id = Auth::user()->id;
-    $seller->first_name = $request->first_name;
-    $seller->last_name = $request->last_name;
-    $seller->document_type = $request->document_type;
-    $seller->document = $request->document;
-    $seller->email = $request->email;
-    $seller->gender = $request->gender;
-    $seller->admission = $request->admission;
-    $seller->save();
-    flash('Seller updated successful.')->success();
-    return redirect()->route('sellers.index');
+    $client->user_id = Auth::user()->id;
+    $client->seller_id = $request->seller_id;
+    $client->first_name = $request->first_name;
+    $client->last_name = $request->last_name;
+    $client->phone = $request->phone;
+    $client->address = $request->address;
+    $client->email = $request->email;
+    $client->save();
+    flash('Client updated successful.')->success();
+    return redirect()->route('clients.index');
   }
 
   public function destroy($id)
   {
-    $seller = Seller::findOrFail($id);
-    $seller->delete();
-    flash('Seller removed successful.')->error();
-    return redirect()->route('sellers.index');
+    $client = Client::findOrFail($id);
+    $client->delete();
+    flash('Client removed successful.')->error();
+    return redirect()->route('clients.index');
   }
 }
